bad42b3a276780e58ece1268778f5016bd3acdc7
joen <598465252@qq.com>
2018-11-23 17:52:53 +0800

    config: change baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  baseUrl: './',
+  baseUrl: process.env.NODE_ENV === 'production' ? '/vue-circle-progress/' : '/',
+  assetsDir: process.env.NODE_ENV === 'production' ? './' : '',
   outputDir: 'docs'
 }
